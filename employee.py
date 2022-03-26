3d1ae8f88a91abbc5bc98c5484719e985eeb016b
Camdyn <camdyn3@protonmail.com>
2022-03-25 22:20:37 -0700

    reformat code for readability

--- employee.py
+++ employee.py
@@ -1,5 +1,10 @@
 # Might update this to use SQL so it's more like a real-world app, but this is just a fun project so I probably won't
 
+# Would've used a class, but I don't remember how OOP works in python so functions are good
+def space():
+    print("")   # Pretty much \n but ez-er to put throughout the program
+
+
 employee_database = [
     {"name": "Jeff",
      "age": 10,
@@ -12,21 +17,27 @@ employee_database = [
 
 def main():
     try:
-        print("""1: Add a new employee\n2: Print all employees
-3: Delete by age\n4: Update salary by name
-5: Close program""")
+        print("-- Main Menu --")
+        print("""
+1: Add a new employee
+2: Print all employees
+3: Delete by age
+4: Update salary by name
+5: Close program
+""")
 
         user_choice = int(input("Enter your choice: "))
         while user_choice < 0 or user_choice > 5:
-            print("Invalid input, try again")
+            print("That input is not within range.\nTry again...")
             user_choice = int(input("Enter your choice: "))
         front_end_handler(user_choice)
     except ValueError:
-        print("ERROR: Unaccepted value type! Try again...\n")
+        print("ERROR: Unaccepted value type!\nTry again...")
         main()
 
 
 def front_end_handler(user_choice):
+    space()
     if user_choice == 1:
         employee_add()  # Add an employee
 
@@ -54,6 +65,7 @@ def employee_add():
         employee_database.append({"name": employee_name, "age": employee_age, "salary": employee_salary})
         print(f"{employee_name} has been successfully added to the database.")
 
+        space()
         main()
 
     except ValueError:
@@ -66,8 +78,11 @@ def employee_add():
 def employee_print():
     if employee_database:
         print("-- Employee list --")
+
         for employee in employee_database:
             print(f"{employee['name']}: {employee['age']} years old, makes ${employee['salary']} yearly.")
+
+        space()
         main()
 
     else:
@@ -80,7 +95,7 @@ def employee_ageism():
         print("-- Ageism --")
         global employee_database    # Using this so I can reassign it to the new list
         # Now, the only problem is that it can possibly be slower for new lists, but I don't imagine it'll
-        # be much slower. Honestly, could be faster
+        # be much slower. Honestly, could actually be faster since I don't have to use .pop or other methods
 
         post_ageism = []
 
@@ -90,14 +105,18 @@ def employee_ageism():
         for employee in employee_database:
             if employee["age"] < lower_range or employee["age"] > upper_range:
                 post_ageism.append(employee)    # append instead of push? M A D G E
+
             else:
-                print(f"{employee['name']} removed")
+                print(f"{employee['name']} has been removed.")
         employee_database = post_ageism
 
+        space()
         main()
 
     except ValueError:
         print("ERROR: Invalid input")
+        print("Try again...")
+
         employee_ageism()
 
 
@@ -107,6 +126,7 @@ def employee_salary_update():
         found = False
         employee_name = input("Enter the employee's name: ")
         employee_salary = int(input(f"Enter {employee_name}'s new salary: "))
+
         for employee in employee_database:
             if employee_name == employee["name"]:
                 employee["salary"] = employee_salary
@@ -114,13 +134,19 @@ def employee_salary_update():
 
         if found:
             print(f"{employee_name}'s salary was successfully updated.")
+
+            space()
             main()
+
         else:
             print("We couldn't find that employee. Try again.")
             employee_salary_update()
 
     except ValueError:
         print("ERROR: Invalid input")
+        print("Try again...")
+
+        employee_salary_update()
 
 
 main()
